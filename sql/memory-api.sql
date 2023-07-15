a5007d7dd167275771de9224ea99ee859acb33b2
memory <memory@qq.com>
2023-07-15 23:15:12 +0800

    基于SpringBoot项目骨架快速搭建后端环境 接口信息数据库表建立完成

---
+++ sql/memory-api.sql
@@ -0,0 +1,17 @@
+-- 接口信息表
+use memory_api;
+create table if not exists interface_info
+(
+    `id` bigint not null auto_increment comment '主键' primary key,
+    `name` varchar(256) not null comment '用户名',
+    `description` varchar(256) null comment '描述',
+    `url` varchar(512) not null comment '接口地址',
+    `requestHeader` text null comment '请求头',
+    `responseHeader` text null comment '响应头',
+    `userId` varchar(256) not null comment '创建人',
+    `status` int default 0 not null comment '接口状态（0 - 关闭， 1 - 开启））',
+    `method` varchar(256) not null comment '请求类型',
+    `createTime` datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    `updateTime` datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `isDelete` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
+) comment '接口信息表';
