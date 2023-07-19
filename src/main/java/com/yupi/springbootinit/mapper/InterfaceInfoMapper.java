7209270c5ba7abc8e40a188da017cb6776213f06
memory <memory@qq.com>
2023-07-19 23:31:39 +0800

    实体类映射 后端接口信息的增、删、改、查接口编写完成

---
+++ src/main/java/com/yupi/springbootinit/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Lenovo
+* @description 针对表【interface_info(接口信息表)】的数据库操作Mapper
+* @createDate 2023-07-19 08:17:01
+* @Entity com.yupi.springbootinit.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/impl/InterfaceInfoServiceImpl.java
@@ -0,0 +1,71 @@
+package com.yupi.springbootinit.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.gson.Gson;
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.constant.CommonConstant;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.exception.ThrowUtils;
+import com.yupi.springbootinit.mapper.InterfaceInfoMapper;
+import com.yupi.springbootinit.model.dto.interfaceInfo.InterfaceInfoQueryRequest;
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+import com.yupi.springbootinit.model.entity.User;
+import com.yupi.springbootinit.model.vo.UserVO;
+import com.yupi.springbootinit.service.InterfaceInfoService;
+import com.yupi.springbootinit.service.UserService;
+import com.yupi.springbootinit.utils.SqlUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 帖子服务实现
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Service
+@Slf4j
+public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, InterfaceInfo> implements InterfaceInfoService {
+    private final static Gson GSON = new Gson();
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 参数校验
+     *
+     * @param interfaceInfo
+     * @param add
+     */
+    @Override
+    public void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add) {
+        if (interfaceInfo == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+    }
+}
+
+
+
+
