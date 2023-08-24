153bc08672b6cf0e7a13ddd892cbe97cd405635f
memory <memory@qq.com>
2023-08-24 14:14:03 +0800

    API接口开放平台 基本功能完成

--- src/main/java/com/memory/memory_api/service/impl/UserInterfaceInfoServiceImpl.java
+++ src/main/java/com/memory/memory_api/service/impl/UserInterfaceInfoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.memory.memory_api.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.memorycommen.model.entity.UserInterfaceInfo;
-import com.memory.memory_api.service.UserInterfaceInfoService;
+import com.memory.memory_api.common.ErrorCode;
+import com.memory.memory_api.exception.BusinessException;
 import com.memory.memory_api.mapper.UserInterfaceInfoMapper;
+import com.memory.memory_api.service.UserInterfaceInfoService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +18,20 @@ import org.springframework.stereotype.Service;
 public class UserInterfaceInfoServiceImpl extends ServiceImpl<UserInterfaceInfoMapper, UserInterfaceInfo>
     implements UserInterfaceInfoService{
 
+    @Override
+    public boolean invokeCount(long interfaceInfoId, long userId) {
+        // 判断
+        if (interfaceInfoId <= 0 || userId <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        UpdateWrapper<UserInterfaceInfo> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("interfaceInfoId", 1);
+        updateWrapper.eq("userId", 1);
+
+//        updateWrapper.gt("leftNum", 0);
+        updateWrapper.setSql("leftNum = leftNum - 1, totalNum = totalNum + 1");
+        return this.update(updateWrapper);
+    }
 }
 
 
---
+++ src/main/java/com/memory/memory_api/service/impl/inner/InnerInterfaceInfoServiceImpl.java
@@ -0,0 +1,35 @@
+package com.memory.memory_api.service.impl.inner;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.memorycommen.model.entity.InterfaceInfo;
+import com.example.memorycommen.service.InnerInterfaceInfoService;
+import com.memory.memory_api.common.ErrorCode;
+import com.memory.memory_api.exception.BusinessException;
+import com.memory.memory_api.mapper.InterfaceInfoMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.dubbo.config.annotation.DubboService;
+
+import javax.annotation.Resource;
+
+/**
+ * 内部接口服务实现类
+ *
+ * @author memory
+ */
+@DubboService
+public class InnerInterfaceInfoServiceImpl implements InnerInterfaceInfoService {
+
+    @Resource
+    private InterfaceInfoMapper interfaceInfoMapper;
+
+    @Override
+    public InterfaceInfo getInterfaceInfo(String url, String method) {
+        if (StringUtils.isAnyBlank(url, method)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        QueryWrapper<InterfaceInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("url", url);
+        queryWrapper.eq("method", method);
+        return interfaceInfoMapper.selectOne(queryWrapper);
+    }
+}
